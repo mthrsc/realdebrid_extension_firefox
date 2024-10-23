a4b0aae41a0ad3a1a68ebec906857303cb1b1d2c
mthrsc <mthrsc@me.com>
2024-10-23 22:20:57 +0100

    commit

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("Extension starting")
+
+credsLoaded = false
+
+let results = browser.storage.local.get("creds")
+results.then(onGot, onError)
+
+function onGot(item){
+    console.log(item)
+    credsLoaded = true
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+if (!credsLoaded){
+    creds = {
+        username: "user",
+        password: "pass",
+        apikey: "apiKey"
+    }
+    browser.storage.local.set({"creds": creds})
+    let results = browser.storage.local.get("creds")
+    results.then(onGot, onError)
+    
+}
+
+browser.storage.local.clear()
