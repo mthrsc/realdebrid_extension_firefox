f9971c6ddd2bf34877ed0762eeaff762e58c1f09
mthrsc <mthrsc@me.com>
2024-10-25 09:37:02 +0100

    Shows in browser

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 console.log("Extension starting")
 
-credsLoaded = false
+// credsLoaded = false
 
-let results = browser.storage.local.get("creds")
-results.then(onGot, onError)
+// let results = browser.storage.local.get("creds")
+// results.then(onGot, onError)
 
-function onGot(item){
-    console.log(item)
-    credsLoaded = true
-}
+// function onGot(item){
+//     console.log(item)
+//     credsLoaded = true
+// }
 
-function onError(error) {
-    console.log(`Error: ${error}`);
-}
+// function onError(error) {
+//     console.log(`Error: ${error}`);
+// }
 
-if (!credsLoaded){
-    creds = {
-        username: "user",
-        password: "pass",
-        apikey: "apiKey"
-    }
-    browser.storage.local.set({"creds": creds})
-    let results = browser.storage.local.get("creds")
-    results.then(onGot, onError)
+// if (!credsLoaded){
+//     creds = {
+//         username: "user",
+//         password: "pass",
+//         apikey: "apiKey"
+//     }
+//     browser.storage.local.set({"creds": creds})
+//     let results = browser.storage.local.get("creds")
+//     results.then(onGot, onError)
     
-}
+// }
 
-browser.storage.local.clear()
+// browser.storage.local.clear()
