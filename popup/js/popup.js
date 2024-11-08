c5f3ec39eac677ce3c0edc715d2a94d6bcc8e906
mthrsc <mthrsc@me.com>
2024-11-08 09:35:00 +0000

    Login screen - value retrieval fix

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -6,36 +6,42 @@ function setValue(name, value) {
     return browser.storage.local.set({ [name]: value });
 }
 
-function getValue(name) {
+async function getValue(name, callback) {
     console.log("[name]: " + [name])
-    return browser.storage.local.get(name).then((result) => {
-        ret =  result[name] !== undefined ? result[name] : "";
-        console.log("ret: " + ret)
-        return ret
+    return await browser.storage.local.get(name).then((result) => {
+        ret = result[name] !== undefined ? result[name] : "";
+        console.log("ret for "+ name + " : " + ret)
+        // return ret
+        callback(ret)
     }).catch((error) => {
         console.error("Error retrieving value for", name, ":", error);
-        return ""; // Return an empty string in case of error
+        // return ""; // Return an empty string in case of error
+        callback("")
     });
 }
 
 
-function updatePopupWithLatestTorrent(apiKey){
+function updatePopupWithLatestTorrent(apiKey) {
 
 }
 
+
 document.addEventListener("DOMContentLoaded", () => {
-    
-    validKey = getValue("validKey")
-    apiKey = getValue("apiKey")
 
-    console.log("validKey: " + validKey)
-    console.log("apiKey: " + apiKey)
+    validKeyPromise = getValue("validKey", (validKeyCallback) => {
+        validKey = validKeyCallback
+        console.log("validKey :", validKey)
+    })
 
-    if(validKey){
+    apiKeyPromise = getValue("apiKey",(apiKeyCallback) => {
+        apiKey = apiKeyCallback
+        console.log("API Key:", apiKey);});
+
+    if (validKey) {
         console.log("validKey is true")
-        
-        //Implement and call updatePopupWithLatestTorrent()
 
+        //Implement and call updatePopupWithLatestTorrent()
+        // RQ
 
 
         // browser.storage.local.get("apiKey").then((result) => {
@@ -53,7 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // loginButton.addEventListener("click", RQ(apiKey, urlLogin));
         loginButton.addEventListener("click", () => {
             apiKey = document.getElementById("apikey").value,
-            RQ(apiKey, urlLogin); // This will now only be called on button click
+                RQ(apiKey, urlLogin); // This will now only be called on button click
         });
     }
 });
--- popup/js/request.js
+++ popup/js/request.js
@@ -1,52 +1,64 @@
-function RQ(apiKey, url) {
+function RQ(apiKey, url, rqType) {
     console.log("RQ apiKey: " + apiKey)
     console.log("RQ url: " + url)
     // Bearer token
     fetch(url, {
         method: 'GET',
-        mode: 'cors', 
+        mode: 'cors',
         headers: {
-            'Authorization': `Bearer ${apiKey}`, 
+            'Authorization': `Bearer ${apiKey}`,
             'Content-Type': 'application/json'
         }
     })
-    .then(response => {
-        // Check if the response is ok (status code in the range 200-299)
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json(); // Parse the JSON from the response
-    })
-    .then(data => {
-        // console.log(data); // Handle the data from the response
-        // format data
+        .then(response => {
+            // Check if the response is ok (status code in the range 200-299)
+            if (!response.ok) {
+                if (rqType === "LOGIN") {
+                    setValue("apiKey", "")
+                    setValue("validKey", false)        
+                    return false
+                } else {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+            }
+            if (rqType === "LOGIN") {
+                setValue("apiKey", apiKey)
+                setValue("validKey", true)    
+                return true
+            } else {
+                return response.json(); // Parse the JSON from the response
+            }
+        })
+        .then(data => {
+            // console.log(data); // Handle the data from the response
+            // format data
 
-        // store apikey
-        setValue("apiKey", apiKey)
-        setValue("validKey", true)
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+            // store apikey
+            setValue("apiKey", apiKey)
+            setValue("validKey", true)
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 
     // Add additional logic here, like validation or storing credentials
 }
 
-function storeapiKey(apiKey){
+function storeapiKey(apiKey) {
     let validKey = false
     browser.storage.local.get("apiKey").then((result) => {
         validKey = result.validKey
     }).catch((error) => {
         validKey = false
-        browser.storage.local.set({"validKey": validKey})
+        browser.storage.local.set({ "validKey": validKey })
     })
 
     browser.storage.local.get("apiKey").then((result) => {
-        if ((result == "" || result == undefined) || (!validKey || validKey == "" || validKey == undefined)){
-            browser.storage.local.set({"apiKey": apiKey})
+        if ((result == "" || result == undefined) || (!validKey || validKey == "" || validKey == undefined)) {
+            browser.storage.local.set({ "apiKey": apiKey })
         }
     }).catch((error) => {
-        browser.storage.local.set({"validKey": validKey})
+        browser.storage.local.set({ "validKey": validKey })
     });
 
 }
